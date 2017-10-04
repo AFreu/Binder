9f01fccd78255f27c1261feec5740b05e555e032
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-10-04 14:03:53 +0200

    pass google account into profilefragment and populate parts of the UI based on this

--- app/src/main/java/com/mobilecomputing/binder/Fragments/ProfileFragment.java
+++ app/src/main/java/com/mobilecomputing/binder/Fragments/ProfileFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.mobilecomputing.binder.R;
 
 
@@ -15,6 +17,8 @@ import com.mobilecomputing.binder.R;
  */
 public class ProfileFragment extends BasicFragment {
 
+    private GoogleSignInAccount userAccount;
+    private TextView nameText;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -25,7 +29,29 @@ public class ProfileFragment extends BasicFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        initUI(view);
+
+        return view;
     }
 
+    public void initUI(View view) {
+        nameText = view.findViewById(R.id.profile_name);
+
+        populateUI();
+    }
+
+    /**
+     * Populates the user interface based on Google user data
+     */
+    public void populateUI() {
+
+        if(userAccount != null) {
+            nameText.setText(userAccount.getDisplayName());
+        }
+    }
+
+    public void setUserAccount(GoogleSignInAccount userAccount) {
+        this.userAccount = userAccount;
+    }
 }
