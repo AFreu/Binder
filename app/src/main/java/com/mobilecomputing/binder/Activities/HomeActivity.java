21cc70c7e9c4993b0a6c185c75dd077cd4e86590
Anton Freudenthaler <anton.p.v.freudenthaler@gmail.com>
2017-10-04 14:49:47 +0200

    Fixed color themes

--- app/src/main/java/com/mobilecomputing/binder/Activities/HomeActivity.java
+++ app/src/main/java/com/mobilecomputing/binder/Activities/HomeActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.mobilecomputing.binder.Fragments.CardFragment;
