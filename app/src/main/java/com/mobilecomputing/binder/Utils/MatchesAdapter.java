84633bb78166c51359ffeeb17a32ca32211d0ee0
lovisajaberg <jaberglovisa@gmail.com>
2017-10-04 14:10:07 +0200

    Added MatchesAdapter

---
+++ app/src/main/java/com/mobilecomputing/binder/Utils/MatchesAdapter.java
@@ -0,0 +1,30 @@
+package com.mobilecomputing.binder.Utils;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Lovis on 04/10/17.
+ */
+
+public class MatchesAdapter extends ArrayAdapter {
+
+
+    public MatchesAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
