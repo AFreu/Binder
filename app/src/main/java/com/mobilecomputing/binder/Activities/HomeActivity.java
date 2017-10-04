fe17fb2abff41fd92f9ee56ef31ac2cf2ca2cd90
Anton Freudenthaler <anton.p.v.freudenthaler@gmail.com>
2017-10-04 10:14:30 +0200

    Started on HomeActivity navigation

--- app/src/main/java/com/mobilecomputing/binder/Activities/HomeActivity.java
+++ app/src/main/java/com/mobilecomputing/binder/Activities/HomeActivity.java
@@ -3,9 +3,14 @@ package com.mobilecomputing.binder.Activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.mobilecomputing.binder.Fragments.CardFragment;
+import com.mobilecomputing.binder.Fragments.MatchesFragment;
+import com.mobilecomputing.binder.Fragments.ProfileFragment;
 import com.mobilecomputing.binder.R;
 
 public class HomeActivity extends BasicActivity {
@@ -17,17 +22,22 @@ public class HomeActivity extends BasicActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_books:
+                    mTextMessage.setText(R.string.title_books);
+                    switchToFragment("CardFragment");
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_matches:
+                    mTextMessage.setText(R.string.title_matches);
+                    switchToFragment("MatchesFragment");
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    switchToFragment("ProfileFragment");
                     return true;
             }
+
             return false;
         }
 
@@ -41,6 +51,30 @@ public class HomeActivity extends BasicActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+    private void switchToFragment(String fragment){
+
+        Fragment frag;
+
+        switch (fragment){
+            case "CardFragment":
+                frag = new CardFragment();
+                break;
+            case "MatchesFragment":
+                frag = new MatchesFragment();
+                break;
+            case "ProfileFragment":
+                frag = new ProfileFragment();
+                break;
+            default:
+                frag = new CardFragment();
+                break;
+        }
+
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.container, frag).commit();
     }
 
 }
