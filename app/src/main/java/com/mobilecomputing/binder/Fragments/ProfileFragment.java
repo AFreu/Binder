fee21573602af8985279d432e6c2f4e32e3aa012
Anton Freudenthaler <anton.p.v.freudenthaler@gmail.com>
2017-10-06 15:24:14 +0200

    Added a library for FlowLayout

--- app/src/main/java/com/mobilecomputing/binder/Fragments/ProfileFragment.java
+++ app/src/main/java/com/mobilecomputing/binder/Fragments/ProfileFragment.java
@@ -6,12 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.mobilecomputing.binder.R;
 import com.squareup.picasso.Picasso;
+import com.wefika.flowlayout.FlowLayout;
 
 
 /**
@@ -22,6 +24,9 @@ public class ProfileFragment extends BasicFragment {
     private GoogleSignInAccount userAccount;
     private TextView nameText;
     private ImageView profileImage;
+    private FlowLayout flowLayout;
+
+    private View.OnClickListener clickRemoveListener;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -33,7 +38,15 @@ public class ProfileFragment extends BasicFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        clickRemoveListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((FlowLayout)view.getParent()).removeView(view);
+            }
+        };
         initUI(view);
+
+
 
         return view;
     }
@@ -41,6 +54,36 @@ public class ProfileFragment extends BasicFragment {
     public void initUI(View view) {
         nameText = view.findViewById(R.id.profile_name);
         profileImage = view.findViewById(R.id.profile_picture);
+
+        flowLayout = view.findViewById(R.id.profile_flowlayout);
+
+
+        TextView t = new TextView(getContext());
+        t.setText("Vadsomhelst!");
+        t.setTextSize(20);
+        t.setOnClickListener(clickRemoveListener);
+        //t.getLayoutParams().width = FlowLayout.LayoutParams.WRAP_CONTENT;
+
+
+        flowLayout.addView(t);
+
+        TextView b = new TextView(getContext());
+        b.setText("En knapp");
+        b.setTextSize(20);
+        b.setOnClickListener(clickRemoveListener);
+
+        //b.getLayoutParams().width = FlowLayout.LayoutParams.WRAP_CONTENT;
+
+        flowLayout.addView(b);
+
+        TextView q = new TextView(getContext());
+        q.setText("hej");
+        q.setTextSize(20);
+        q.setOnClickListener(clickRemoveListener);
+
+        flowLayout.addView(q);
+
+        flowLayout.postInvalidate();
 
         populateUI();
     }
@@ -54,6 +97,7 @@ public class ProfileFragment extends BasicFragment {
             nameText.setText(userAccount.getDisplayName());
             Picasso.with(getContext()).load(userAccount.getPhotoUrl()).into(profileImage);
         }
+
     }
 
     public void setUserAccount(GoogleSignInAccount userAccount) {
@@ -61,4 +105,8 @@ public class ProfileFragment extends BasicFragment {
 
         populateUI();
     }
+
+
+
+
 }
