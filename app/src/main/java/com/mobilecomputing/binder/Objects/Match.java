5bf677be71027179875cb87670525c469020a65d
JimmyHoldo <mpq@MPQ.local>
2017-10-11 16:26:44 +0200

    calculate percent

--- app/src/main/java/com/mobilecomputing/binder/Objects/Match.java
+++ app/src/main/java/com/mobilecomputing/binder/Objects/Match.java
@@ -3,22 +3,25 @@ package com.mobilecomputing.binder.Objects;
 import java.io.Serializable;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by Lovis on 04/10/17.
  */
 
-public class Match implements Serializable {
+public class Match implements Serializable, Comparator<Match> {
     public String name;
     public Integer age;
     public String city;
-    public String id;
+    public int id;
     public String pictureUrl;
     public Integer percent;
     public List<Book> books = new ArrayList<>();
 
-    public Match (String name, Integer age, String city, String id, String picture, Integer percent){
+    public Match(){}
+
+    public Match (String name, Integer age, String city, int id, String picture, Integer percent){
         this.name = name;
         this.age = age;
         this.city = city;
@@ -33,5 +36,9 @@ public class Match implements Serializable {
 
     public List<Book> getBooks() {
         return books;
+    }
+
+    public int compare(Match left, Match right) {
+        return right.percent.compareTo(left.percent);
     }
 }
